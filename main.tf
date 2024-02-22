d1d38d2add9380c3aa9225c483d2dc8869fdcbff
Sandesh Reddy Tummala <tummala.san@northeastern.edu>
2024-02-21 23:50:42 -0500

    update

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "google" {
 resource "google_compute_network" "vpc" {
   name                    = var.vpc_name
   auto_create_subnetworks = false
-  delete_default_routes_on_create  true
+  delete_default_routes_on_create  = true
   routing_mode            = "REGIONAL"
 }
 
