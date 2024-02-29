7b96e78923204c95ed9f746a4c500319fda77828
Sandesh Reddy Tummala <tummala.san@northeastern.edu>
2024-02-29 03:06:52 -0500

    db_creation

---
+++ startup-script.sh
@@ -0,0 +1,14 @@
+DB_HOST=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/db_host" -H "Metadata-Flavor: Google")
+DB_USER=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/db_user" -H "Metadata-Flavor: Google")
+DB_PASSWORD=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/db_password" -H "Metadata-Flavor: Google")
+
+# Create application.properties at /tmp 
+cat << EOF > /tmp/application.properties 
+server.port=8080
+spring.datasource.url=jdbc:mysql://${DB_HOST}:3306/webapp?createDatabaseIfNotExist=true
+spring.datasource.username=${DB_USER}
+spring.datasource.password=${DB_PASSWORD}
+spring.jpa.hibernate.ddl-auto=create-drop
+spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect
+spring.jpa.show-sql=true
+EOF
