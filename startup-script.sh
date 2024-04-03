8f9a7466ce99275896191c2923bad7daf4c02b83
Sandesh Reddy Tummala <tummala.san@northeastern.edu>
2024-04-03 17:09:58 -0400

    update autoscaler and load balancing

--- startup-script.sh
+++ startup-script.sh
@@ -8,7 +8,7 @@ server.port=8080
 spring.datasource.url=jdbc:mysql://${DB_HOST}:3306/webapp?createDatabaseIfNotExist=true
 spring.datasource.username=${DB_USER}
 spring.datasource.password=${DB_PASSWORD}
-spring.jpa.hibernate.ddl-auto=create-drop
+spring.jpa.hibernate.ddl-auto=update
 spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect
 spring.jpa.show-sql=true
 logging.file.path=./
